c9990db9d353669dc771b02a487891c48cd5c6a3
kamal kishore <kamal.kishore981@gmail.com>
2024-11-02 14:32:53 -0400

    used useState to store the task item list and add event listner to add button. on click of add button , A new task will be added to the list

--- React-Todo-App/src/App.jsx
+++ React-Todo-App/src/App.jsx
@@ -2,18 +2,32 @@ import AppName from "./components/Appname";
 import AddTodo from "./components/AddTodo";
 import "./App.css";
 import TodoItems from "./components/TodoItems";
+import { useState } from "react";
 function App() {
   // Create data
-  const todoItems = [
-    { name: "Buy React course", dueDate: "10/20/2024" },
-    { name: "Buy Html course", dueDate: "10/21/2024" },
-    { name: "Buy javaScript course", dueDate: "10/22/2024" },
-    { name: "Buy html course", dueDate: "10/22/2024" },
-  ];
+  // const todoItems = [
+  //   { name: "Buy React course", dueDate: "10/20/2024" },
+  //   { name: "Buy Html course", dueDate: "10/21/2024" },
+  //   { name: "Buy javaScript course", dueDate: "10/22/2024" },
+  //   { name: "Buy html course", dueDate: "10/22/2024" },
+  // ];
+
+  let [todoItems, setTodoItems] = useState([]);
+
+  let displayToDoItems = (taskName, taskDueDate) => {
+    const newTodoItems = [
+      ...todoItems,
+      {
+        name: taskName,
+        dueDate: taskDueDate,
+      },
+    ];
+    setTodoItems(newTodoItems);
+  };
   return (
     <center className="todo-container">
       <AppName></AppName>
-      <AddTodo />
+      <AddTodo displayToDoItems={displayToDoItems} />
       <TodoItems todoItems={todoItems}></TodoItems>
     </center>
   );
--- React-Todo-App/src/components/AddTodo.jsx
+++ React-Todo-App/src/components/AddTodo.jsx
@@ -1,16 +1,45 @@
-function AddTodo() {
+import { useState } from "react";
+
+function AddTodo({ displayToDoItems }) {
+  let [taskName, setTaskName] = useState();
+  let [taskDueDate, setTaskDueDate] = useState();
+
+  let handleInputChange = (event) => {
+    setTaskName(event.target.value);
+  };
+
+  let handleDateChange = (event) => {
+    setTaskDueDate(event.target.value);
+  };
+
+  let handleAddButtonClick = () => {
+    displayToDoItems(taskName, taskDueDate);
+    setTaskName("");
+    setTaskDueDate("");
+  };
   return (
     // add bootstrap grid layout
     <div className="container">
       <div className="row kg-row">
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo here" />
+          <input
+            type="text"
+            placeholder="Enter Todo here"
+            value={taskName}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="col-4">
-          <input type="date" />
+          <input type="date" value={taskDueDate} onChange={handleDateChange} />
         </div>
         <div className="col-2">
-          <button type="button" className="btn btn-success kg-button">
+          <button
+            type="button"
+            className="btn btn-success kg-button"
+            onClick={() => {
+              handleAddButtonClick();
+            }}
+          >
             Add
           </button>
         </div>
