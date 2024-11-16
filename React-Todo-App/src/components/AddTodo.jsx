3107986e2f6f89bd117049c64aa8d9571ee3ad8b
kamal kishore <kamal.kishore981@gmail.com>
2024-11-15 20:35:04 -0500

    replace useState with useRef inside form element in AddTodo component

--- React-Todo-App/src/components/AddTodo.jsx
+++ React-Todo-App/src/components/AddTodo.jsx
@@ -1,22 +1,20 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { MdAddTask } from "react-icons/md";
 function AddTodo({ displayToDoItems }) {
-  let [taskName, setTaskName] = useState("");
-  let [taskDueDate, setTaskDueDate] = useState("");
+  // let [taskName, setTaskName] = useState("");
+  // let [taskDueDate, setTaskDueDate] = useState("");
 
-  let handleInputChange = (event) => {
-    setTaskName(event.target.value);
-  };
+  let toDoNameElement = useRef();
+  let dueDateElement = useRef();
 
-  let handleDateChange = (event) => {
-    setTaskDueDate(event.target.value);
-  };
   // on click on add button calls handleAddButtonClick function which is calling displayToDoItems function
   let handleAddButtonClick = (event) => {
     event.preventDefault();
-    displayToDoItems(taskName, taskDueDate);
-    setTaskName("");
-    setTaskDueDate("");
+    const todoName = toDoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    toDoNameElement.current.value = "";
+    dueDateElement.current.value = "";
+    displayToDoItems(todoName, dueDate);
   };
   return (
     // add bootstrap grid layout
@@ -26,12 +24,11 @@ function AddTodo({ displayToDoItems }) {
           <input
             type="text"
             placeholder="Enter Todo here"
-            value={taskName}
-            onChange={handleInputChange}
+            ref={toDoNameElement}
           />
         </div>
         <div className="col-4">
-          <input type="date" value={taskDueDate} onChange={handleDateChange} />
+          <input type="date" ref={dueDateElement} />
         </div>
         <div className="col-2">
           <button className="btn btn-success kg-button">
