fe2151efed09141a99b604b65f289c11eb157e80
kamal kishore <kamal.kishore981@gmail.com>
2024-11-20 13:35:46 -0500

    replace usestate in App.js with useReducer hook

--- React-Todo-App/src/App.jsx
+++ React-Todo-App/src/App.jsx
@@ -3,34 +3,52 @@ import AddTodo from "./components/AddTodo";
 import "./App.css";
 import TodoItems from "./components/TodoItems";
 import WelcomeMessage from "./components/WelcomeMessage";
-import { useState } from "react";
+import { useReducer } from "react";
 import { TodoItemsContext } from "./store/todo-items-store";
+// useReducer takes a pure function as an argument called reducer function. reducer function takes current value and action object and returns the updated state.
+const todoItemsReducer = (currentTodoItems, action) => {
+  let newToDoItems = currentTodoItems;
+  if (action.type === "NEW_ITEM") {
+    newToDoItems = [
+      ...currentTodoItems,
+      { name: action.payload.taskName, dueDate: action.payload.taskDueDate },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newToDoItems = currentTodoItems.filter((item) => {
+      return item.name !== action.payload.todoName;
+    });
+  }
+  return newToDoItems;
+};
+
 function App() {
-  // states for the app
-  const [todoItems, setTodoItems] = useState([]);
+  // const [todoItems, setTodoItems] = useState();
+  // replacing useState with useReducer for complex state management .
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
   //function to display items
   const addNewItem = (taskName, taskDueDate) => {
-    setTodoItems((currentValue) => {
-      const newTodoItems = [
-        ...currentValue,
-        {
-          name: taskName,
-          dueDate: taskDueDate,
-        },
-      ];
-      return newTodoItems;
-    });
+    //earlier we were updating states by directly passing the value , later we are passing an anonymous function with the previous value as an argument  to get the latest value of the state to update it with setTodoItems.
+    //newItemAction is an object which holds value given by the user.
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        taskName,
+        taskDueDate,
+      },
+    };
+    //  dispatch function dispatch the action object to reducer function to update the state value.
+    dispatchTodoItems(newItemAction);
   };
 
-  // handle delete button function.
+  // handle delete button.
   const deleteItem = (todoName) => {
-    // console.log(todoName);
-    // create newTaskArray after delete items from array
-    const newTaskArray = todoItems.filter((item) => {
-      return item.name !== todoName;
-    });
-    // resetting the array after deleting the item from current items array
-    setTodoItems(newTaskArray);
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        todoName: todoName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
   };
   return (
     <TodoItemsContext.Provider
